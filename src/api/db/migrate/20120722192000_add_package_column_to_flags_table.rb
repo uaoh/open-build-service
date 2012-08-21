155fecbca9a41e3c4ee33996be43a7362ce2fb04
David Greaves <david@dgreaves.com>
2012-08-21 17:34:43 +0100

    Correct the api db migration
    Signed-off-by: David Greaves <david@dgreaves.com>

--- src/api/db/migrate/20120722192000_add_package_column_to_flags_table.rb
+++ src/api/db/migrate/20120722192000_add_package_column_to_flags_table.rb
@@ -1,9 +1,9 @@
-class AddFlagsPackageColumn < ActiveRecord::Migration
-  def up
+class AddPackageColumnToFlagsTable < ActiveRecord::Migration
+  def self.up
     add_column :flags, :package, :binary, :limit => 256, :null => true
   end
 
-  def down
+  def self.down
     remove_column :flags, :package
   end
 end
