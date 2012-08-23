595a955a21e540a6be1b738a22396f1514b70d37
David Greaves <david@dgreaves.com>
2012-08-23 09:52:37 +0100

    Remove validation from Configuration screen

--- src/api/app/controllers/configurations_controller.rb
+++ src/api/app/controllers/configurations_controller.rb
@@ -4,7 +4,7 @@ class ConfigurationsController < ApplicationController
   before_filter :require_admin, :only => [:update]
 
   validate_action :show => {:method => :get, :response => :configuration}
-  validate_action :update => {:method => :put, :request => :configuration}
+#  validate_action :update => {:method => :put, :request => :configuration}
 
   # GET /configuration
   # GET /configuration.json
