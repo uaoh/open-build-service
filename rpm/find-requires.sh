0b10b00dfe85d5cb60d2f6947b793badf78e2ff7
David Greaves <david@dgreaves.com>
2018-10-15 16:46:50 +0100

    Fix the requires??
    Signed-off-by: David Greaves <david.greaves@jolla.com>

--- rpm/find-requires.sh
+++ rpm/find-requires.sh
@@ -12,12 +12,12 @@ tdir=`mktemp -d`
 # extract files
 tar xJf $sourcearchive -C $tdir >&/dev/null
 
-(echo tdir
+(echo tdir $tdir
  ls -laF $tdir
  echo tdir/
  ls -laF $tdir/*) 1>&2
 
-pushd $tdir/*/*/src/api >& /dev/null  || echo "___ERROR_SRCDIR_NOT_FOUND___ $tdir/*"
+pushd $tdir/*/src/api >& /dev/null  || echo "___ERROR_SRCDIR_NOT_FOUND___ $tdir/*"
 ruby.ruby2.5 -rbundler -e 'exit' || echo "___ERROR_BUNDLER_NOT_INSTALLED___"
 
 mode="resolve"
